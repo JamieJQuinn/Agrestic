d310016c334b9de7365633ab36a21547da6db684
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-11 10:45:14 +0100

    Add entry point and fill in fabric.mod.json

---
+++ src/main/java/com/jamiejquinn/agrestic/Agrestic.java
@@ -0,0 +1,10 @@
+package com.jamiejquinn.agrestic;
+
+import com.agrestic.api.ModInitializer;
+
+public class Agrestic implements ModInitializer {
+  @Override
+  public void onInitialize() {
+
+  }
+}
