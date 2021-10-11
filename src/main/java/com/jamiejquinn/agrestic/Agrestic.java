463dba65136bb50532704a4b987774d1ab2ba03c
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-11 11:28:16 +0100

    Removed mixins, added sample logging

--- src/main/java/com/jamiejquinn/agrestic/Agrestic.java
+++ src/main/java/com/jamiejquinn/agrestic/Agrestic.java
@@ -1,10 +1,14 @@
 package com.jamiejquinn.agrestic;
 
-import com.agrestic.api.ModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Agrestic implements ModInitializer {
+  public static final Logger LOGGER = LogManager.getLogger(Constants.MODID);
+
   @Override
   public void onInitialize() {
-
+    LOGGER.info("Hello Fabric world!");
   }
 }
---
+++ src/main/java/com/jamiejquinn/agrestic/Constants.java
@@ -0,0 +1,5 @@
+package com.jamiejquinn.agrestic;
+
+public class Constants {
+    public static String MODID ="agrestic";
+}
