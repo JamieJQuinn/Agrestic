e627bee49e0e8f5bf8715b214e884c2d624b3eb7
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-17 13:12:45 +0100

    Add item group to creative tab

--- src/main/java/com/jamiejquinn/agrestic/Agrestic.java
+++ src/main/java/com/jamiejquinn/agrestic/Agrestic.java
@@ -1,8 +1,11 @@
 package com.jamiejquinn.agrestic;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
@@ -11,9 +14,15 @@ import org.apache.logging.log4j.Logger;
 public class Agrestic implements ModInitializer {
   public static final Logger LOGGER = LogManager.getLogger(Constants.MODID);
 
-  public static final ExampleItem EXAMPLE_ITEM = new ExampleItem(new FabricItemSettings().group(ItemGroup.MISC));
+  public static final ItemGroup AGRESTIC_GROUP = FabricItemGroupBuilder.create(
+          new Identifier(Constants.MODID, "general"))
+          .icon(() -> new ItemStack(Items.BOWL))
+          .appendItems(stacks -> {
+            stacks.add(new ItemStack(Agrestic.EXAMPLE_ITEM));
+          })
+          .build();
 
-  
+  public static final ExampleItem EXAMPLE_ITEM = new ExampleItem(new FabricItemSettings().group(AGRESTIC_GROUP));
 
   @Override
   public void onInitialize() {
